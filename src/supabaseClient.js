aab8aee31a7d53cef885b340ed93fe6e18f12120
Patrick Schönewolf <103068924+F1r0x@users.noreply.github.com>
2026-01-01 18:01:38 +0100

    Delete src/supabaseClient.js

--- src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = "https://dpyjybphkorwfuopftyi.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRweWp5YnBoa29yd2Z1b3BmdHlpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcyNjgzNTEsImV4cCI6MjA4Mjg0NDM1MX0.f8cHLHGHb_gBmp_FuXR3OUwvJp80-tAGulcyelA_1Ps";
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
